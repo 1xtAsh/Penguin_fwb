20f3b79de201ddf0570471a7cc6e4ef089176073
Adhitya Mohan <me@adhityamohan.in>
2022-12-21 11:02:54 +0530

    IS_DEBUGGABLE -> IS_ENG
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1495,7 +1495,7 @@ public final class SystemServer implements Dumpable {
                 PackageManager.FEATURE_VR_MODE_HIGH_PERFORMANCE);
 
         // For debugging RescueParty
-        if (Build.IS_DEBUGGABLE && SystemProperties.getBoolean("debug.crash_system", false)) {
+        if (Build.IS_ENG && SystemProperties.getBoolean("debug.crash_system", false)) {
             throw new RuntimeException();
         }
 
@@ -1707,7 +1707,7 @@ public final class SystemServer implements Dumpable {
 
             mSystemServiceManager.startService(ActivityTriggerService.class);
 
-            if (Build.IS_DEBUGGABLE && ProfcollectForwardingService.enabled()) {
+            if (Build.IS_ENG && ProfcollectForwardingService.enabled()) {
                 t.traceBegin("ProfcollectForwardingService");
                 mSystemServiceManager.startService(ProfcollectForwardingService.class);
                 t.traceEnd();
